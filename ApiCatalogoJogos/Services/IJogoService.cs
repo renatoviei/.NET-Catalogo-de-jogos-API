e569c6ea26a1b218b3e942ea57204c905728d3c2
Renato <renatoviei@gmail.com>
2021-07-28 09:33:24 -0300

    Subindo parte do projeto
    Subindo controller, entidades, services, exceptions e interface do repositório.

---
+++ ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid Id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid Id, JogoInputModel jogo);
+        Task Atualizar(Guid Id, double preco);
+        Task Remover(Guid Id);
+    }
+}
